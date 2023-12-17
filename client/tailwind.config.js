d95aaf50b725813f7d05067307b07aba311f6e97
Shamim <mdshamimreza5552@gmail.com>
2023-12-17 21:07:22 +0600

    Add poppins font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [],
-}
+};
 
