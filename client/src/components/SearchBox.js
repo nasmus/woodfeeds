7b1330bf1557dd3c6194cf8954f0b7f1098d31b3
Shamim <mdshamimreza5552@gmail.com>
2023-12-27 02:20:10 +0600

    Add search seggestion (not completed)

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,33 +1,59 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SearchSuggestList from './SearchSuggestList';
 
 export default function SearchBox() {
     const navigate = useNavigate();
     const [query, setQuery] = useState('');
+    const [suggestion, setSuggestion] = useState([]);
+
     const submitHendler = (e) => {
         e.preventDefault();
         navigate(query ? `/search?query=${query}`: '/search' );
     };
 
+    const HandleChange = (e) => {
+        setQuery(e);
+        getData(e);
+    }
+
+    
+        async function getData(value) {
+            try {
+                const { data } = await axios.get('/api/products');
+                const result = data.filter((rez) => {
+                    return value && rez && rez.name.includes(value)
+                });
+                setSuggestion(result);
+                console.log(suggestion);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
   return (
     <div>
-        <div className="align-self-center">
-            <div className="header-search">
-                <form className="ec-btn-group-form" onSubmit={submitHendler} >
-                    <input 
-                        name='q'
-                        id='q'
-                        className="form-control ec-search-bar" 
-                        placeholder="Search products..." 
-                        type="text" 
-                        aria-describedby='button-search'
-                        aria-label='search product'
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                    <button className="submit" id='button-search' type="submit"><i className="fi-rr-search"></i></button>
-                </form>
-            </div>
+      <div className="align-self-center">
+        <div className="header-search">
+          <form className="ec-btn-group-form" onSubmit={submitHendler}>
+            <input
+              name="q"
+              id="q"
+              className="form-control ec-search-bar"
+              placeholder="Search products..."
+              type="text"
+              aria-describedby="button-search"
+              aria-label="search product"
+              onChange={(e) => HandleChange(e.target.value)}
+            />
+            <button className="submit" id="button-search" type="submit">
+              <i className="fi-rr-search"></i>
+            </button>
+          </form>
         </div>
+          {suggestion && suggestion.length > 0 ? <SearchSuggestList suggestion={suggestion} /> : ''}
+      </div>
     </div>
-  )
+  );
 }
---
+++ client/src/components/SearchSuggestList.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function SearchSuggestList({suggestion}) {
+  return (
+    
+    <div className="bg-slate-100 drop-shadow-lg rounded-md absolute z-10">
+    {suggestion.map((suggest) => {
+      return (
+        <Link className="!text-black no-underline" to={`/product/${suggest.slug}`}>
+          <div className="flex p-4 w-[300px] max-w-[300px] lg:w-[500px] lg:max-w-[500px] hover:bg-slate-200 transition-colors">
+            <img
+              src={`/images/${suggest.image}`}
+              alt="img"
+              className="me-3 w-10 object-scale-down"
+            />
+            <p>{suggest.name}</p>
+          </div>
+        </Link>
+      );
+        })}
+        
+      
+    </div>
+  );
+}
