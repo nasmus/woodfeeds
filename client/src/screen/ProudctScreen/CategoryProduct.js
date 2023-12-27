bde0c8c7ec378d2b48cf02144c4e68f3df068246
nasmus <nasmus.shahadat@gmail.com>
2023-12-27 19:48:03 +0600

    top rated product and search

--- client/src/screen/ProudctScreen/CategoryProduct.js
+++ client/src/screen/ProudctScreen/CategoryProduct.js
@@ -5,10 +5,10 @@ import axios from "axios";
 function CategoryProduct() {
   const [productsBookShelf, setProductsBookShelf] = useState([]);
   const displayProduct = productsBookShelf.slice(0, 10);
-  const [prodcutCornarShelf, setProductCornerShelf] = useState([])
-  const displayCornerShelf = prodcutCornarShelf.slice(0, 10)
-  const [prodcutBadSideShelf, setProductBadSideShelf] = useState([])
-  const displayBadSideShelf = prodcutBadSideShelf.slice(0, 10)
+  const [prodcutCornarShelf, setProductCornerShelf] = useState([]);
+  const displayCornerShelf = prodcutCornarShelf.slice(0, 10);
+  const [prodcutBadSideShelf, setProductBadSideShelf] = useState([]);
+  const displayBadSideShelf = prodcutBadSideShelf.slice(0, 10);
 
   useEffect(() => {
     const fatchData = async () => {
@@ -36,21 +36,62 @@ function CategoryProduct() {
 
   return (
     <div className=" mt-2 mb-2 ">
-      <h2 className=" m-2 ">Book Shelf </h2>
+      <div className="flex justify-between items-center px-3 py-2 bg-slate-100 mx-1 rounded-md">
+        <h3 className="">Book Shelf</h3>
+        <button
+          type="button"
+          class="px-3 py-2 text-xs font-medium text-center text-white bg-cyan-500 rounded-lg hover:bg-cyan-700 focus:outline-none "
+        >
+         View More
+        </button>
+      </div>
       <div className="product-grid">
         {displayProduct.map((product) => (
           <Product product={product}></Product>
         ))}
       </div>
 
-      <h2 className="ml-2">Wall Corner</h2>
+      <div className="flex gap-3 p-4 ">
+        <div>
+          <img
+            className="object-cover w-full h-auto rounded-tl-xl rounded-bl-xl"
+            src="https://media.e-valy.com/cms/banners/a71ef5d2-45ed-46c5-b2ab-a9071e8efe26"
+            alt="Small1"
+          />
+        </div>
+        <div>
+          <img
+            className="object-cover w-full h-auto rounded-br-xl rounded-tr-xl"
+            src="https://media.e-valy.com/cms/banners/a71ef5d2-45ed-46c5-b2ab-a9071e8efe26"
+            alt="Small"
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center px-3 py-2 bg-slate-100 mx-1 rounded-md">
+        <h3 className="">Wall Corner</h3>
+        <button
+          type="button"
+          class="px-3 py-2 text-xs font-medium text-center text-white bg-cyan-500 rounded-lg hover:bg-cyan-700 focus:outline-none "
+        >
+         View More
+        </button>
+      </div>
       <div className="product-grid">
         {displayCornerShelf.map((product) => (
           <Product product={product}></Product>
         ))}
       </div>
 
-      <h2 className="ml-2">Bad Side Shelf</h2>
+      <div className="flex justify-between items-center px-3 py-2 bg-slate-100 mx-1 rounded-md">
+        <h3 className="">Bad Shide shelf</h3>
+        <button
+          type="button"
+          class="px-3 py-2 text-xs font-medium text-center text-white bg-cyan-500 rounded-lg hover:bg-cyan-700 focus:outline-none "
+        >
+         View More
+        </button>
+      </div>
       <div className="product-grid">
         {displayBadSideShelf.map((product) => (
           <Product product={product}></Product>
--- client/src/screen/ProudctScreen/TopRatedProduct.js
+++ client/src/screen/ProudctScreen/TopRatedProduct.js
@@ -16,13 +16,21 @@ function TopRatedProduct() {
   }, []);
 
   return (
-    <div className=' mt-2 mb-2 '>
-        <h2 className=' m-2 ' >Top Rated Product</h2>
-        <div className="product-grid">
-            {displayProduct.map((product) => (
-              <Product product={product}></Product>
-            ))}
-          </div>
+    <div className=" mt-2 mb-2 ">
+      <div className="flex justify-between items-center px-3 py-2 bg-slate-100 mx-1 rounded-md">
+        <h3 className="">Top Rated Product</h3>
+        <button
+          type="button"
+          class="px-3 py-2 text-xs font-medium text-center text-white bg-cyan-500 rounded-lg hover:bg-cyan-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
+        >
+         View More
+        </button>
+      </div>
+      <div className="product-grid mt-2">
+        {displayProduct.map((product) => (
+          <Product product={product}></Product>
+        ))}
+      </div>
     </div>
   );
 }
