05ad4c7eb46039e8eed50cd49e4b0ffb9589d53f
nasmus <nasmus.shahadat@gmail.com>
2023-12-30 00:53:39 +0600

    pagination complete in category product view page

--- client/src/screen/CategoryPage.js
+++ client/src/screen/CategoryPage.js
@@ -6,29 +6,55 @@ import Product from "../components/Product";
 function CategoryPage() {
   const [categoryValue, setCategoryValue] = useState([]);
   const param = useParams();
-  
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(4);
 
   useEffect(() => {
     const fatchData = async () => {
-      const category = await axios.get(`/api/category/${param.id}/${param.slug}`);
+      const category = await axios.get(
+        `/api/category/${param.id}/${param.slug}`
+      );
       if (category) {
         setCategoryValue(category.data);
       }
     };
     fatchData();
-  }, [param.id,param.slug]);
+  }, [param.id, param.slug]);
+
+  // Calculate the indexes for the products to be displayed on the current page
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = categoryValue.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div>
       <div className="product-grid2">
-        {categoryValue.length > 0 ? (
-        categoryValue.map((product) => {
-          return <Product product={product}></Product>;
-        })
+        {currentProducts.length > 0 ? (
+          currentProducts.map((product) => {
+            return <Product product={product}></Product>;
+          })
         ) : (
-            <div className="min-h-[50vh]">
-              <h3 className="text-center">No Product found!</h3>
-            </div>
+          <div className="min-h-[50vh]">
+            <h3 className="text-center">No Product found!</h3>
+          </div>
         )}
+      </div>
+
+      {/* Pagination */}
+      <div className="flex justify-center items-center">
+        {Array.from({
+          length: Math.ceil(categoryValue.length / productsPerPage),
+        }).map((_, index) => (
+          <button className="px-3 py-2 text-xs font-medium text-center text-white bg-cyan-500 rounded-lg hover:bg-cyan-700  focus:outline-none focus:ring-blue-300 " key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
       </div>
     </div>
   );
