6429c2491b408b201d694917b24240322fd81633
Shamim <mdshamimreza5552@gmail.com>
2024-01-04 02:27:47 +0600

    Fix order  details page design

--- admin/src/AdminScreen/OrderDetails.js
+++ admin/src/AdminScreen/OrderDetails.js
@@ -1,10 +1,10 @@
-import React,{ useContext, useReducer, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useReducer, useState } from 'react';
 
-import { Store } from '../Store'
-import { useParams } from "react-router-dom";
 import axios from "axios";
-import { getError } from "../utils";
+import { useParams } from "react-router-dom";
 import Sidebar from '../Component/Sidebar';
+import { Store } from '../Store';
+import { getError } from "../utils";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -71,34 +71,36 @@ function OrderDetails() {
   }, [orderId, userInfo.token]);
 
   return (
-    <div style={{ paddingLeft: "200px" }}>
-      <Sidebar />
+    <div className='lg:pl-52'>
+      <div className='hidden lg:block'>
+        <Sidebar />
+      </div>
 
       <div className="flex items-center justify-between px-5 py-5">
         <h1 className="text-4xl text-center text-cyan-500 font-bold">
           Order Details
         </h1>
-        <div className="flex md:flex-col md:w-1/5">
-        <form onSubmit={handleSubmit}>
-          <select
-            name="rderStatus"
-            id="status"
-            value={orderStatus}
-            onChange={(e) => setOrderStatus(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:px-6 p-2 "
-          >
-            <option value="Processing">Processing</option>
-            <option value="Shipped">Shipped</option>
-            <option value="Received">Received</option>
-          </select>
-          <button className="bg-green-500 w-full hover:bg-green-600 p-2 ml-2 md:ml-0 md:px-6 md:my-1.5 text-white rounded-lg">
-            Submit
-          </button>
-        </form>
+        <div className="flex md:flex-col lg:w-1/5">
+          <form className='flex flex-col items-center' onSubmit={handleSubmit}>
+            <select
+              name="rderStatus"
+              id="status"
+              value={orderStatus}
+              onChange={(e) => setOrderStatus(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full md:px-6 p-2 "
+            >
+              <option value="Processing">Processing</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Received">Received</option>
+            </select>
+            <button className="bg-green-500 w-full hover:bg-green-600 p-2 ml-2 md:ml-0 md:px-6 md:my-1.5 text-white rounded-lg">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
 
-      <div className="px-5 py-5 md:flex w-10/12 ">
+      <div className="px-5 py-5 md:flex w-full lg:w-10/12 ">
         <section className="md:w-4/5 md:px-6 text-black">
           <div className="relative overflow-x-auto py-3 md:py-6">
             <table className="w-full text-sm text-left rtl:text-right text-black font-medium">
@@ -123,41 +125,40 @@ function OrderDetails() {
               </thead>
               {orderDetail.orderItems &&
                 orderDetail.orderItems.map((item) => {
-                   
-                    return (
-                      <tbody>
-                        <tr className="bg-white border-b ">
-                          <th
-                            scope="row"
-                            className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                          >
-                            <img
-                              src={`/images/${item.image}`}
-                              alt=""
-                              className="w-8 h-8"
-                            />
-                            <span className="pl-2">{item.name}</span>
-                          </th>
-                          <td className="px-6 py-4">{item._id}</td>
-                          <td className="px-6 py-4 ">
-                            <span>{item.quantity}</span>
-                          </td>
-                          <td className="px-6 py-4">
-                            {item.price * item.quantity}
-                          </td>
-                          <td className="px-6 py-4">
-                          <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{item.orderStatus}</span>
-                            
-                          </td>
-                        </tr>
-                      </tbody>
-                    );
-                  
+                  return (
+                    <tbody>
+                      <tr className="bg-white border-b ">
+                        <th
+                          scope="row"
+                          className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                        >
+                          <img
+                            src={`/images/${item.image}`}
+                            alt=""
+                            className="w-8 h-8"
+                          />
+                          <span className="pl-2">{item.name}</span>
+                        </th>
+                        <td className="px-6 py-4">{item._id}</td>
+                        <td className="px-6 py-4 ">
+                          <span>{item.quantity}</span>
+                        </td>
+                        <td className="px-6 py-4">
+                          {item.price * item.quantity}
+                        </td>
+                        <td className="px-6 py-4">
+                          <span class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                            {item.orderStatus}
+                          </span>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
                 })}
             </table>
           </div>
         </section>
-        <section className="">
+        <section className="py-6">
           <div className="bg-gradient-to-tr from-gray-700 to-gray-600 text-white rounded-lg w-11/12 p-5">
             <h1 className="text-4xl  font-bold">Address Info.</h1>
             <div className="pt-6">
@@ -220,7 +221,7 @@ function OrderDetails() {
         </section>
       </div>
     </div>
-  )
+  );
 }
 
 export default OrderDetails
