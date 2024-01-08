a741d6e035d48d6e63b7f1344e108f62f7ba44a8
nasmus <nasmus.shahadat@gmail.com>
2024-01-08 16:25:44 +0600

    user login using phone number

--- client/src/screen/SignUp/SignInScreen.js
+++ client/src/screen/SignUp/SignInScreen.js
@@ -11,6 +11,7 @@ function SignInScreen() {
   const redirectUrl = new URLSearchParams(search).get("redirect");
   const redirect = redirectUrl ? redirectUrl : "/";
   const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState('');
   const [password, setPassword] = useState("");
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
@@ -19,7 +20,7 @@ function SignInScreen() {
     e.preventDefault();
     try {
       const { data } = await Axios.post("/api/users/signin", {
-        email,
+        phone,
         password,
       });
       ctxDispatch({ type: "USER_SIGNIN", payload: data });
@@ -52,19 +53,19 @@ function SignInScreen() {
               >
                 <div>
                   <label
-                    for="email"
+                    for="phone"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Your email
+                    Your Phone Number
                   </label>
                   <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    controlId="email"
+                    type="phone"
+                    name="phone"
+                    id="phone"
+                    onChange={(e) => setPhone(e.target.value)}
+                    
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    placeholder="name@company.com"
+                    placeholder="01712000000"
                     required
                   />
                 </div>
